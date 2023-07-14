22b58676fa7106d4542f5e94fb00d45b4d0fc24d
gabrielmlaker <mlakergabriel@gmail.com>
2023-07-14 20:56:47 -0300

    Payment, one-to-one association

--- src/main/java/com/example/demo/repositories/OrderItemRepository.java
+++ src/main/java/com/example/demo/repositories/OrderItemRepository.java
@@ -3,8 +3,9 @@ package com.example.demo.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entities.OrderItem;
+import com.example.demo.entities.pk.OrderItemPK;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 	
 	
 
